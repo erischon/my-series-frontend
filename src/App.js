8d731c0f1ef4986b3a09ee673bb1d588ca389835
Eri Schon <erischon@gmail.com>
2022-07-03 12:19:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import AddSerie from "./pages/AddSerie";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/add" element={<AddSerie />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
