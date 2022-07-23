18ecc0a32c57528d69ee2c2ab004d09f549bced9
Eri Schon <erischon@gmail.com>
2022-07-23 18:50:20 +0200

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 const Input = (props) => {
-  const { name, overview } = props.infos;
+  const { name, overview, backdrop_path } = props.infos;
+
+  let imgUrl =
+    "https://images.unsplash.com/photo-1573399054516-90665ecc44be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1975&q=80";
+
+  if (backdrop_path) {
+    imgUrl = `https://image.tmdb.org/t/p/w500${backdrop_path}`;
+  }
+
   return (
     <div className="input-result">
       <input type="radio" value={name} name="serie" />
-      <label>{name}</label>
+      <div className="input-result__image">
+        <img src={imgUrl} alt={name} />
+        <label>{name}</label>
+      </div>
       {overview ? <p>Description : {overview}</p> : ""}
     </div>
   );
